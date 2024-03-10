72354317878fae4b769bdef2004400b474f57806
tejashvi-t <tejashvi2812@gmail.com>
2024-03-10 19:19:02 +0530

    creating own lib

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
+  const username = "tejashvi";
+
   return (
     <>
-      <h1>Chai aur React with vite || TC </h1>
+      <h1>Chai aur React with vite || {username} </h1>
     </>
   );
 }
-
 export default App;
+
+//  {username } => called evaluated expression
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>custom my App</h1>
+//     </div>
+//   );
+// }
+//  the old way of react to add element/tag
+
+// const anotherUser = "chai or react";
+
+// const reactElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://www.facebook.com",
+//     target: "_blank",
+//   },
+//   "click me to visit facebook",
+//   anotherUser   => {using a var in old method }
+// );
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-
-
